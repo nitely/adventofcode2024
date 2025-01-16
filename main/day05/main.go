77377137d2cd60f048cd77351627ac52166ffc3c
nitely <ecastroborsani@gmail.com>
2025-01-16 10:28:39 -0300

    day05

---
+++ main/day05/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"adventofgo2024/main/utils"
+	"fmt"
+	"strings"
+)
+
+func part1(rules map[int]map[int]bool, pages [][]int) {
+	result := 0
+	for _, page := range pages {
+		pageOk := true
+		for i, num := range page {
+			for _, n := range page[i+1:] {
+				if !rules[num][n] {
+					pageOk = false
+				}
+			}
+		}
+		if pageOk {
+			result += page[len(page)/2]
+		}
+	}
+	fmt.Println(result)
+}
+
+func part2(rules map[int]map[int]bool, pages [][]int) {
+	result := 0
+	for _, page := range pages {
+		pageOk := true
+		for i, num := range page {
+			for _, n := range page[i+1:] {
+				if !rules[num][n] {
+					pageOk = false
+				}
+			}
+		}
+		if !pageOk {
+			//sort.Slice(page, func(i, j int) bool {
+			//	return rules[page[i]][page[j]]
+			//})
+			page2 := utils.Sorted(page, func(i, j int) bool {
+				return rules[i][j]
+			})
+			result += page2[len(page2)/2]
+		}
+	}
+	fmt.Println(result)
+}
+
+func main() {
+	s := utils.MustReadFile("input.txt")
+	lines := strings.Split(s, "\n")
+	rules := make(map[int]map[int]bool)
+	pages := make([][]int, 0)
+	for _, line := range lines {
+		switch {
+		case strings.Contains(line, "|"):
+			parts := strings.Split(line, "|")
+			utils.Assert(len(parts) == 2)
+			a := utils.Atoi(parts[0])
+			b := utils.Atoi(parts[1])
+			if rules[a] == nil {
+				rules[a] = make(map[int]bool)
+			}
+			rules[a][b] = true
+		case line != "":
+			parts := strings.Split(line, ",")
+			utils.Assert(len(parts) > 1)
+			pages = append(pages, []int{})
+			ln := len(pages) - 1
+			for _, n := range parts {
+				pages[ln] = append(pages[ln], utils.Atoi(n))
+			}
+		}
+	}
+	part1(rules, pages)
+	part2(rules, pages)
+	fmt.Println("ok")
+}
--- main/utils/misc.go
+++ main/utils/misc.go
@@ -1,7 +1,19 @@
 package utils
 
+import "sort"
+
 func Assert(condition bool) {
 	if !condition {
 		panic("assertion")
 	}
+}
+
+func Sorted[T any](original []T, less func(i, j T) bool) []T {
+	// return sorted copy instead of in-place sort
+	copySlice := make([]T, len(original))
+	copy(copySlice, original)
+	sort.Slice(copySlice, func(i, j int) bool {
+		return less(copySlice[i], copySlice[j])
+	})
+	return copySlice
 }
